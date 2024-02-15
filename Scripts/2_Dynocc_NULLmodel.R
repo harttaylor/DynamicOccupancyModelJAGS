f7875bb95b49215e88de2dea5a525b796362cfce
Taylor Hart <tah@ualberta.ca>
2024-02-15 14:33:13 -0700

    Adding turnover plots

--- Scripts/2_Dynocc_NULLmodel.R
+++ Scripts/2_Dynocc_NULLmodel.R
@@ -34,22 +34,22 @@ J <- do.call(rbind, lapply(1:dim(y)[1], function(i){
 win.data <- list(y = y, nsite = dim(y)[1], nyear = dim(y)[2], nsurv = nsurv, J = J)
 
 # Parameters monitored
-params <- c("psi1", "phi", "gamma", "n.occ", "growthr", "turnover")
+params <- c("psi1", "phi", "gamma", "tau", "n.occ", "growthr", "turnover")
 
 # MCMC settings
-ni <- 1000
+ni <- 100
 nt <- 1
-nb <- 500
+nb <- 50
 nc <- 3
 
 # Call JAGS from R and run the model 
 system.time({
-  out <- jags(data = win.data, inits = inits, parameters.to.save = params, 
-              model.file = "cl_model_basic.txt", n.chains = nc, 
+  out_tau <- jags(data = win.data, inits = inits, parameters.to.save = params, 
+              model.file = "cl_model_NULL.txt", n.chains = nc, 
               n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE)
 })
 
-print(out)
+print(out_tau)
 
 # Make quick plots
 df1 <- data.frame(Year = 1994:2017, Gamma = out$mean$gamma)
@@ -111,33 +111,36 @@ ggsave("Results/occupancy_plot.png", occupancy_plot, dpi = 300, width = 8, heigh
 # Plot turnover rates 
 # Assuming 'out_tau_edge$mean$tau' is your model output
 # Extract the mean turnover rates for each year
-mean_turnover <- c(out_tau_edge$mean$tau$V2['Mean'], 
-                   out_tau_edge$mean$tau$V3['Mean'], 
-                   out_tau_edge$mean$tau$V4['Mean'],
-                   out_tau_edge$mean$tau$V5['Mean'], 
-                   out_tau_edge$mean$tau$V6['Mean'], 
-                   out_tau_edge$mean$tau$V7['Mean'], 
-                   out_tau_edge$mean$tau$V8['Mean'],
-                   out_tau_edge$mean$tau$V9['Mean'], 
-                   out_tau_edge$mean$tau$V10['Mean'], 
-                   out_tau_edge$mean$tau$V11['Mean'], 
-                   out_tau_edge$mean$tau$V12['Mean'],
-                   out_tau_edge$mean$tau$V13['Mean'], 
-                   out_tau_edge$mean$tau$V14['Mean'], 
-                   out_tau_edge$mean$tau$V15['Mean'], 
-                   out_tau_edge$mean$tau$V16['Mean'],
-                   out_tau_edge$mean$tau$V17['Mean'], 
-                   out_tau_edge$mean$tau$V18['Mean'], 
-                   out_tau_edge$mean$tau$V19['Mean'], 
-                   out_tau_edge$mean$tau$V20['Mean'],
-                   out_tau_edge$mean$tau$V21['Mean'], 
-                   out_tau_edge$mean$tau$V22['Mean'], 
-                   out_tau_edge$mean$tau$V23['Mean'], 
-                   out_tau_edge$mean$tau$V24['Mean'],
-                   out_tau_edge$mean$tau$V25['Mean'])
-print(out_tau_edge$mean$tau[,1])
+print(summary(out_tau_edge$mean$tau))
+mean(out_tau_edge$mean$tau[,1])
+
+mean_turnover <- c(mean(out_tau_edge$mean$tau[,1]), 
+                   mean(out_tau_edge$mean$tau[,2]), 
+                   mean(out_tau_edge$mean$tau[,3]),
+                   mean(out_tau_edge$mean$tau[,4]), 
+                   mean(out_tau_edge$mean$tau[,5]), 
+                   mean(out_tau_edge$mean$tau[,6]), 
+                   mean(out_tau_edge$mean$tau[,7]), 
+                   mean(out_tau_edge$mean$tau[,8]), 
+                   mean(out_tau_edge$mean$tau[,9]),
+                   mean(out_tau_edge$mean$tau[,10]), 
+                   mean(out_tau_edge$mean$tau[,11]), 
+                   mean(out_tau_edge$mean$tau[,12]),
+                   mean(out_tau_edge$mean$tau[,13]), 
+                   mean(out_tau_edge$mean$tau[,14]), 
+                   mean(out_tau_edge$mean$tau[,15]),
+                   mean(out_tau_edge$mean$tau[,16]), 
+                   mean(out_tau_edge$mean$tau[,17]), 
+                   mean(out_tau_edge$mean$tau[,18]), 
+                   mean(out_tau_edge$mean$tau[,19]), 
+                   mean(out_tau_edge$mean$tau[,20]), 
+                   mean(out_tau_edge$mean$tau[,21]),
+                   mean(out_tau_edge$mean$tau[,22]), 
+                   mean(out_tau_edge$mean$tau[,23]), 
+                   mean(out_tau_edge$mean$tau[,24]))
+
 # Create a data frame for plotting
-years <- 1:25  # Adjust if your study years are named differently
+years <- 1:24  # Adjust if your study years are named differently
 turnover_data <- data.frame(Year = years, MeanTurnover = mean_turnover)
 
 # Plot
@@ -147,5 +150,122 @@ ggplot(turnover_data, aes(x = Year, y = MeanTurnover)) +
   labs(title = "Changes in Turnover Over 25 Years",
        x = "Year",
        y = "Mean Turnover") +
-  theme_minimal()
+  theme_classic()
 
+ggsave("Results/YearlyTurnover.png")
+
+
+# Convert the matrix to a long format for use with ggplot
+turnover_data <- reshape2::melt(out_tau_edge$mean$tau)
+
+# Name the columns appropriately
+names(turnover_data) <- c("Site", "Year", "TurnoverRate")
+write.csv(turnover_data, "Data/turnover_data.csv")
+# Plotting the heatmap
+ggplot(turnover_data, aes(x = Year, y = Site, fill = TurnoverRate)) +
+  geom_tile() +  # Use geom_tile for heatmap
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = median(turnover_data$TurnoverRate, na.rm = TRUE), 
+                       name = "Turnover Rate") +
+  labs(title = "Heatmap of Turnover Rate Across Sites and Years",
+       x = "Year",
+       y = "Site") +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))  # Rotate x-axis labels if needed
+ggsave("Results/Heatmap_Turnover.png")
+# In a population with high turnover, species are frequently going locally extinct and are being replaced by other species colonizing the area. 
+# This could be due to environmental variability, habitat fragmentation, or other disturbances.
+# A high turnover might suggest instability in the local population. This could be driven by environmental factors (like resource availability or habitat quality), 
+# social factors (such as competition or predation), or anthropogenic factors (like urban development or environmental pollution).
+# Reflection of Habitat Quality: In wildlife populations, high turnover might reflect changes or fluctuations in habitat quality. 
+# Good-quality habitats tend to have lower turnover rates as individuals are more likely to stay and reproduce, whereas in poorer quality habitats, 
+# individuals might leave in search of better conditions, or there might be higher mortality rates.
+
+
+
+# Create heatmap on actual study area map 
+# Load necessary libraries
+library(ggplot2)
+library(sf)
+library(dplyr)
+library(ggmap)
+library(grid)
+
+# Read your data (adjust the file paths as necessary)
+coords_data <- read.csv("Data/latslons.csv")
+turnover_data <- read.csv("Data/turnover_data.csv")
+
+# Calculate the average turnover for each site
+average_turnover <- turnover_data %>%
+  group_by(Site) %>%
+  summarize(AverageTurnover = mean(TurnoverRate, na.rm = TRUE))
+
+# Merge the average turnover data with the coordinates data
+merged_data <- merge(coords_data, average_turnover, by = "Site")
+
+# Save the merged data to a new CSV file
+# write.csv(merged_data, "Data/average_site_turnover.csv", row.names = FALSE)
+
+# Convert to a simple features (sf) object
+coordinates <- st_as_sf(merged_data, coords = c('X', 'Y'), crs = 4326)
+
+
+
+
+# Load points data with treatment column
+points_data <- read.csv("Data/latslons.csv")
+
+
+
+seismic_lines_clipped <- readRDS("Human footprint extraction/seismic_lines_clipped.rds")
+pipelines_clipped <- readRDS("Human footprint extraction/pipelines_clipped.rds")
+roads_clipped <- readRDS("Human footprint extraction/roads_clipped.rds")
+harvest_clipped <- readRDS("Human footprint extraction/harvest_clipped.rds")
+
+# Calculate the bounding box
+xmin <- min(points_data$X)
+xmax <- max(points_data$X)
+ymin <- min(points_data$Y)
+ymax <- max(points_data$Y)
+
+# Manually expand the bounding box
+expand_factor <- 0.2  # Adjust this factor as needed
+xrange <- xmax - xmin
+yrange <- ymax - ymin
+expanded_bbox <- c(xmin - xrange * expand_factor, ymin - yrange * expand_factor, 
+                   xmax + xrange * expand_factor, ymax + yrange * expand_factor)
+
+# Convert expanded_bbox to a named vector
+bbox_named <- setNames(expanded_bbox, c("left", "bottom", "right", "top"))
+
+# Transform all spatial data to WGS 84
+crs_wgs84 <- st_crs(4326)
+coordinates <- st_transform(coordinates, crs_wgs84)
+seismic_lines_clipped <- st_transform(seismic_lines_clipped, crs_wgs84)
+pipelines_clipped <- st_transform(pipelines_clipped, crs_wgs84)
+roads_clipped <- st_transform(roads_clipped, crs_wgs84)
+harvest_clipped <- st_transform(harvest_clipped, crs_wgs84)
+
+# Fetch the terrain map image
+terrain_map <- get_map(location = bbox_named, source = "google", maptype = "terrain", crop = FALSE)
+terrain_raster <- rasterGrob(as.raster(terrain_map), interpolate = TRUE)
+
+# Plot with the terrain map as the background
+map_plot <- ggplot() +
+  annotation_custom(terrain_raster, xmin = bbox_named["left"], xmax = bbox_named["right"], ymin = bbox_named["bottom"], ymax = bbox_named["top"]) +
+  geom_sf(data = seismic_lines_clipped, color = "gray", size = 0.1) +
+  geom_sf(data = harvest_clipped, color = "grey", fill = "white", size = 1) +
+  geom_sf(data = pipelines_clipped, color = "gray", size = 1) +
+  geom_sf(data = roads_clipped, color = "gray", size = 1) +
+  geom_sf(data = coordinates, aes(color = AverageTurnover), size = 3) +
+  scale_color_viridis_c(option = "C", direction = -1, name = "Average\nTurnover") +
+  coord_sf(xlim = c(minx, maxx), ylim = c(miny, maxy), expand = FALSE) +
+  labs(title = '',
+       x = 'Longitude', 
+       y = 'Latitude') +
+  theme_minimal() +
+  theme(legend.position = "right",
+        legend.key.width = unit(1.5, "cm"),
+        legend.key.height = unit(0.5, "cm"))
+print(map_plot)
+ggsave("Results/YearlyMeanTurnover.png", dpi = 300)
